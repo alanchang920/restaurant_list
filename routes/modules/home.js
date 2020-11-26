98e8a8d642997839f8ac1081d407bd7e40b7eab5
alanchang920 <73819875+alanchang920@users.noreply.github.com>
2020-11-26 21:28:12 +0800

    Update home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,18 +13,75 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   let keyword = req.query.keyword.trim()
-  // find   
+
   Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
     .lean()
     .then(restaurants => {
-      // exception
+
       if (!restaurants.length) {
         keyword = `你的收藏沒有"${keyword}"的相關項目唷！`
       }
-      // do the searching
+
       res.render('index', { restaurants, keyword, css: 'index.css' })
     })
     .catch(error => console.log(error))
 })
+
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'asc' })
+    .then(restaurants => res.render('index', {
+      restaurants, css: 'index.css'
+    }))
+    .catch(error => console.log(error))
+})
+
+
+router.get('/asc', (req, res) => {
+  const sort = req.path
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'asc' })
+    .then(restaurants => res.render('index', {
+      restaurants, css: 'index.css', sort
+    }))
+    .catch(error => console.log(error))
+})
+
+router.get('/desc', (req, res) => {
+  const sort = req.path
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'desc' })
+    .then(restaurants => res.render('index', {
+      restaurants, css: 'index.css'
+    }))
+    .catch(error => console.log(error))
+})
+
+router.get('/location', (req, res) => {
+  const sort = req.path
+  Restaurant.find()
+    .lean()
+    .sort({ location: 'asc' })
+    .then(restaurants => res.render('index', {
+      restaurants, css: 'index.css'
+    }))
+    .catch(error => console.log(error))
+})
+
+
+router.get('/category', (req, res) => {
+  const sort = req.path
+  Restaurant.find()
+    .lean()
+    .sort({ category: 'asc' })
+    .then(restaurants => res.render('index', {
+      restaurants, css: 'index.css'
+    }))
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
